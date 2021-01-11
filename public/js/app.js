b77576203d0792b3034b0868b7c467591538228f
Jasper Frumau <jasper@imagewize.com>
2021-01-11 15:34:08 +0700

    several publications

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/jasper/code/smt-laravel-demo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jasper/code/smt-laravel-demo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
